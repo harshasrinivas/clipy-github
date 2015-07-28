f61763690741cc853dc3bc8a01c029120969dafd
Tushar Gautam <tushar.rishav@gmail.com>
2015-07-28 17:06:42 +0530

    PEP8 indent rule conformed

--- cli_github/utils.py
+++ cli_github/utils.py
@@ -22,81 +22,81 @@ API_TOKEN = os.environ.get('GITHUB_TOKEN')
 
 
 def exception():
-	"""runs everytime an exception is caught"""
-	print("Enter a valid URL. For help, type 'cli-github -h'")
-	sys.exit(0)
+    """runs everytime an exception is caught"""
+    print("Enter a valid URL. For help, type 'cli-github -h'")
+    sys.exit(0)
 
 
 # URL PARSING
 
 def url_parse(name):
-	"""parse urls with different prefixes"""
-	position = name.find("github.com")
-	if position >= 0:
-		if position != 0:
-			position_1 = name.find("www.github.com")
-			position_2 = name.find("http://github.com")
-			position_3 = name.find("https://github.com")
-			if position_1*position_2*position_3 != 0:
-				exception()
-				sys.exit(0)
-		name = name[position+11:]
-		if name.endswith('/'):
-			name = name[:-1]
-		return name
-	else:
-		if name.endswith('/'):
-			name = name[:-1]
-		return name
+    """parse urls with different prefixes"""
+    position = name.find("github.com")
+    if position >= 0:
+        if position != 0:
+            position_1 = name.find("www.github.com")
+            position_2 = name.find("http://github.com")
+            position_3 = name.find("https://github.com")
+            if position_1*position_2*position_3 != 0:
+                exception()
+                sys.exit(0)
+        name = name[position+11:]
+        if name.endswith('/'):
+            name = name[:-1]
+        return name
+    else:
+        if name.endswith('/'):
+            name = name[:-1]
+        return name
 
 # GET TO OBTAIN JSON
 
 
 def get_req(url):
-	"""simple get request"""
-	request = urllib.request.Request(url)
-	request.add_header('Authorization', 'token %s' % API_TOKEN)
-	try:
-		response = urllib.request.urlopen(request).read().decode('utf-8')
-		return response
-	except urllib.error.HTTPError:
-		exception()
-		sys.exit(0)
+    """simple get request"""
+    request = urllib.request.Request(url)
+    request.add_header('Authorization', 'token %s' % API_TOKEN)
+    try:
+        response = urllib.request.urlopen(request).read().decode('utf-8')
+        return response
+    except urllib.error.HTTPError:
+        exception()
+        sys.exit(0)
 
 # RETURNS 302
 
 
 def geturl_req(url):
-	"""get request that returns 302"""
-	request = urllib.request.Request(url)
-	request.add_header('Authorization', 'token %s' % API_TOKEN)
-	try:
-		response_url = urllib.request.urlopen(request).geturl()
-		return response_url
-	except urllib.error.HTTPError:
-		exception()
-		sys.exit(0)
+    """get request that returns 302"""
+    request = urllib.request.Request(url)
+    request.add_header('Authorization', 'token %s' % API_TOKEN)
+    try:
+        response_url = urllib.request.urlopen(request).geturl()
+        return response_url
+    except urllib.error.HTTPError:
+        exception()
+        sys.exit(0)
 
 # FETCH JOBS
 
 def show_job(job=None, location=None):
-	fields = ["Company", "Title", "Company"]
-	table = PrettyTable(["Url", "Title", "Company"])
-	table.padding_width = 1
-	table.vertical_char = '.'
-	table.column_width = 20
-	if job:
-		url=GITHUB_JOBS+"?description="+job
-	else:
-		url=GITHUB_JOBS
-	for i in fields:
-		table.align[i] = "c"
-	try:
-		print("Fetching data..\n")
-		jsondata = urllib.request.urlopen(url).read().decode('utf-8')
-		jsondata=json.loads(jsondata)
-		for i in jsondata:
-			table.add_row([i['company_url'], i['title'], i['company']])
-		print(table)
-	except Exception as e:
-		print(e)
+    fields = ["Company", "Title", "Company"]
+    table = PrettyTable(["Url", "Title", "Company"])
+    table.padding_width = 1
+    table.vertical_char = '.'
+    table.column_width = 20
+    if job:
+        url=GITHUB_JOBS+"?description="+job
+    else:
+        url=GITHUB_JOBS
+    for i in fields:
+        table.align[i] = "c"
+    try:
+        print("Fetching data..\n")
+        jsondata = urllib.request.urlopen(url).read().decode('utf-8')
+        jsondata=json.loads(jsondata)
+        for i in jsondata:
+            table.add_row([i['company_url'], i['title'], i['company']])
+        print(table)
+    except Exception as e:
+        print(e)
